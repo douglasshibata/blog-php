cd5c5827f39a221fffb719ca24155b9675680a4f
Douglas Shibata <douglas.shibata11@gmail.com>
2021-11-22 12:34:46 -0300

    Adicionando conexão com banco e consultas

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
-include 'Artigo.php';
-$artigo = new Artigo();
+require 'config.php';
+
+include 'src/Artigo.php';
+$artigo = new Artigo($mysql);
 $artigos = $artigo->exibirTodos();
 
 ?>
@@ -19,7 +21,7 @@ $artigos = $artigo->exibirTodos();
         <h1>Meu Blog</h1>
         <?php foreach ($artigos as $artigo) : ?>
         <h2>
-            <a href="<?php echo $artigo['link']; ?>">
+            <a href="artigo.php?id=<?php echo $artigo['id']; ?>">
                 <?php echo $artigo['titulo']; ?>
             </a>
         </h2>
